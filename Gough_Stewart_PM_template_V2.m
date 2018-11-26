ba4c3284db608eb676176196311433147835768c
sumreenr <sumreen.r@gmail.com>
2018-11-26 05:48:34 -0800

    Added FK stuff

--- Gough_Stewart_PM_template_V2.m
+++ Gough_Stewart_PM_template_V2.m
@@ -180,6 +180,7 @@ for i=1:6
     FK(2,i) = -L*cosd(Betad(i))- Lnorm(i)*(cosd(Betad(i))*cosd(theta2(1,i)) - sind(Betad(i))*cosd(theta1(1,i))*sind(theta2(1,i)));
     FK(3,i) = Lnorm(i)*sind(theta1(1,i))*sind(theta2(1,i));
 end
+%% Do we need to use this stuff?
 mid_12 = (FK(:,4)+FK(:,5))/2;
 avg_b0p0 = mean(FK,2);
 pv = mid_12 - avg_b0p0; 
@@ -187,6 +188,8 @@ pv = pv/norm(pv); %y' axis of platform
 pw = cross(FK(:,4),FK(:,5)); 
 pw = pw/norm(pw); %z' axis of platform
 pu = cross(pv,pw); %x' axis of platform
+
+%[~,~,qd,~] = Fk_practice(Lnorm,x0, y0, z0, p0pi(1,1), p0pi(2,1), b0bi(1,1), b0bi(2,1), alpha*pi/180, beta*pi/180, gamma*pi/180);
 
 %Not sure about this part 
 % p_cosRoll = dot(pu,[1,0,0]')/norm(pu); 
